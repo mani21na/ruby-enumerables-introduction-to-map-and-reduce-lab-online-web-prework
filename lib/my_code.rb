23aa96562851b7bab1f0b86c95d1e6b77a286853
Hanna Lee <mani21na@gmail.com>
2019-07-15 16:38:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,14 +15,22 @@ def map_to_square(source_array)
   source_array.map{|n| n ** 2}
 end
 
-def reduce_to_total(source_array, starting_point = 0)
-  source_array.reduce(starting_point){ |sum, num| sum + num }
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
 end
 
-def reduce_to_all_true(source_array)
-    source_array.reduce {|beTrue| 
-    
-    }
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
 end
 
 def reduce_to_any_true(source_array)
