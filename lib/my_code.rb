924fec417335337c64e750bf971cabdcfa3ac4cd
Hanna Lee <mani21na@gmail.com>
2019-07-15 16:42:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,10 +20,19 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-    source_array.reduce {|beTrue| 
-    
-    }
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
 end
 
 def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
 end
